22122f847ba2b8eee40fad6ce6df81553f53ae1a
Виктор Кочнев <victor_k02@mail.ru>
2023-11-21 10:04:12 +0300

    Добавил класс User

--- app/src/main/java/com/example/boolreader/domain/User.java
+++ app/src/main/java/com/example/boolreader/domain/User.java
@@ -12,6 +12,22 @@ public class User {
         this.pass = pass;
     }
 
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
--- app/src/main/java/com/example/boolreader/services/AuthService.java
+++ app/src/main/java/com/example/boolreader/services/AuthService.java
@@ -1,23 +1,28 @@
 package com.example.boolreader.services;
 
+import com.example.boolreader.domain.User;
+
 import java.security.InvalidParameterException;
 import java.util.HashMap;
 import java.util.Objects;
+import java.util.Optional;
 
 public class AuthService {
     private static AuthService instance;
     public static AuthService getInstance() {
         return Objects.isNull(instance) ? instance = new AuthService() : instance;
     }
-    private HashMap<String, String> users = new HashMap<>();
+    private HashMap<String, User> users = new HashMap<>();
+    private Optional<User> currentUser;
     private AuthService() {
-        users.put("Victor", "pass");
+        users.put("Victor", new User("Victor", "pass"));
     }
 
     public void auth(String login, String pass) {
-        if (!Objects.equals(users.getOrDefault(login, ""), pass)) {
+        if (!users.containsKey(login) || !users.get(login).getPass().equals(pass)) {
             throw new InvalidParameterException("Логин или пароль неверный");
         }
+        currentUser = Optional.of(users.get(login));
     }
 
     public void register(String login, String pass) {
@@ -27,10 +32,13 @@ public class AuthService {
         if (users.containsKey(login)) {
             throw new InvalidParameterException("Такой логин уже занят");
         }
-        users.put(login, pass);
+        users.put(login, new User(login, pass));
     }
 
-    public void getCurrentUser() {
-
+    public User getCurrentUser() {
+        return currentUser.orElseThrow(() -> new RuntimeException("Пользователь не авторизован"));
+    }
+    public boolean isAuth() {
+        return currentUser.isPresent();
     }
 }
