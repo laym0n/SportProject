bd11975721eae486bda8b022a85e324e7eebd9a9
Виктор Кочнев <victor_k02@mail.ru>
2023-11-25 14:26:36 +0300

    Поправил визуал

--- app/src/main/java/com/example/boolreader/MyCompaniesActivity.kt
+++ app/src/main/java/com/example/boolreader/MyCompaniesActivity.kt
@@ -4,8 +4,12 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ListView
+import android.widget.TextView
+import com.example.boolreader.services.CompanyService
 
 class MyCompaniesActivity : AppCompatActivity() {
+//    private val companyService: CompanyService = CompanyService.getInstance();
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_companies)
@@ -21,5 +25,17 @@ class MyCompaniesActivity : AppCompatActivity() {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
+
+//        val myCompaniesListView: ListView = findViewById(R.id.myCompaniesListView)
+//
+//        val textView1 : TextView = TextView(this);
+//        textView1.setText("test");
+//        myCompaniesListView.addView(textView1);
+//
+//        companyService.owningCompaniesOfCurrentUser.forEach({
+//            val textView : TextView = TextView(this);
+//            textView.setText("test");
+//            myCompaniesListView.addView(textView);
+//        })
     }
 }
