a51b785e1a076f5ae7c5a89d8ffe6352b98810b6
Виктор Кочнев <victor_k02@mail.ru>
2023-11-25 15:15:59 +0300

    Сделал окно подписок

--- app/src/main/java/com/example/boolreader/MySubscriptionActivity.kt
+++ app/src/main/java/com/example/boolreader/MySubscriptionActivity.kt
@@ -2,10 +2,15 @@ package com.example.boolreader
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.boolreader.domain.Company
+import com.example.boolreader.services.CompanyService
 
 class MySubscriptionActivity : AppCompatActivity() {
+    private val companyService: CompanyService = CompanyService.getInstance();
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_subscription)
@@ -16,5 +21,9 @@ class MySubscriptionActivity : AppCompatActivity() {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
+
+        val myCompaniesListView: ListView = findViewById(R.id.mySubscriptionsListView)
+        val arrayAdapter : ArrayAdapter<Company> = ArrayAdapter<Company>(this, android.R.layout.simple_list_item_1, companyService.subscribedCompaniesOfCurrentUser)
+        myCompaniesListView.adapter = arrayAdapter;
     }
 }
