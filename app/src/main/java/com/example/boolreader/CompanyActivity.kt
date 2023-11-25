9cb953e2dad575bbc4773ce72d57c4a5baa4fffb
Виктор Кочнев <victor_k02@mail.ru>
2023-11-25 15:25:13 +0300

    Создал структуру окна компании

--- app/src/main/java/com/example/boolreader/CompanyActivity.kt
+++ app/src/main/java/com/example/boolreader/CompanyActivity.kt
@@ -2,8 +2,10 @@ package com.example.boolreader
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.boolreader.services.CompanyService
 
 class CompanyActivity : AppCompatActivity() {
+    private val companyService: CompanyService = CompanyService.getInstance();
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_company)
