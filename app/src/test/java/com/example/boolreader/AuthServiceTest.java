74665954a203c683a59143c2d977a652057eba2b
Виктор Кочнев <victor_k02@mail.ru>
2023-12-05 09:04:06 +0300

    тесты

--- app/src/test/java/com/example/boolreader/AuthServiceTest.java
+++ app/src/test/java/com/example/boolreader/AuthServiceTest.java
@@ -1,5 +1,6 @@
 package com.example.boolreader;
 
+import com.example.boolreader.domain.User;
 import com.example.boolreader.services.AuthService;
 
 import org.junit.Assert;
@@ -13,6 +14,10 @@ public class AuthServiceTest {
     public void testAuth() {
         //Action
         SUT.auth("Victor", "pass");
+
+        //Assert
+        User currentUser = SUT.getCurrentUser();
+        Assert.assertEquals("Victor", currentUser.getLogin());
     }
 
     @Test
@@ -24,6 +29,11 @@ public class AuthServiceTest {
     public void testRegister() {
         //Action
         SUT.register("Victor1", "pass");
+
+        //Assert
+        SUT.auth("Victor1", "pass");
+        User currentUser = SUT.getCurrentUser();
+        Assert.assertEquals("Victor1", currentUser.getLogin());
     }
 
     @Test
